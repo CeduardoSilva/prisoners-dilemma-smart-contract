88439fbe1a386c97b672bfd4504ed9508e7a40db
ceduardo <cs.eduardo@icloud.com>
2022-05-04 10:41:17 -0300

    fixes typo

--- prisoners-dilema.sol
+++ prisoners-dilema.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.12;
 
-contract PrisionersDilemma {
+contract PrisonersDilemma {
 
     uint8 previousChoice;
     address payable previousPlayerAdress;
